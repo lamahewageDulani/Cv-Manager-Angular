782b5fca692baedb88497aaf70f4b7bb97f360e2
lamahewageDulani <dulaniruwanthika99@gmail.com>
2024-02-14 15:02:37 +0530

    login using local storage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,4 +8,24 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 })
 export class LoginComponent {
 
+  email: string = "";
+  password: string = "";
+  path: string = "";
+
+  onClick(){
+    let storedEmail = localStorage.getItem('email');
+    let storedPassword = localStorage.getItem('password');
+    console.log(storedEmail);
+    console.log(storedPassword);
+    console.log(this.email);
+    console.log(this.password);
+
+  if (this.email === storedEmail && this.password === storedPassword) {
+    alert('You are logged in.');
+    this.path = '/Dashboard';
+  } else {
+    alert('Invalid credentials.');
+    this.path = '/Login';
+  }
+  }
 }
